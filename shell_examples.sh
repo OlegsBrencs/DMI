8f14053c871025a58d4ce377d4afadbc5cb7f433
rastamaha1998@gmail.com <rastamaha1998@gmail.com>
2017-09-27 14:13:08 +0300

    20170920_14_10

---
+++ shell_examples.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+#3. piemers
+NAME="Vards Uzvards"
+echo $NAME
+unset NAME
+echo $NAME
+
+#2. piemers
+#NAME="Vards Uzvards"
+#readonly NAME
+#echo $NAME
+#NAME="Vards Uzvards"
+#echo $NAME
+
+#1. piemers
+#NAME="Vards Uzvards"
+#echo $NAME
+
+#0. piemers
+#history > history_20170927.txt
+
